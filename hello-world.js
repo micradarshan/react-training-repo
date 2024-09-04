43a2e0dc9cfa717fe3b2d2925e1b6e6859e5ddf6
Darshan <darshan.micrasol@gmail.com>
2024-09-04 17:26:49 +0530

    Simple JavaScript program
    Experiment with different data types

---
+++ hello-world.js
@@ -0,0 +1,33 @@
+// Print "Hello, World!"
+console.log("Hello, World!");
+
+// 1. String
+const myString = "This is a string";
+console.log("String:", myString);
+
+// 2. Number
+const myNumber = 42;
+console.log("Number:", myNumber);
+
+// 3. Boolean
+const myBoolean = true;
+console.log("Boolean:", myBoolean);
+
+// 4. Null
+const myNull = null;
+console.log("Null:", myNull);
+
+// 5. Undefined
+let myUndefined;
+console.log("Undefined:", myUndefined);
+
+// 6. Object
+const myObject = {
+  name: "John",
+  age: 30,
+};
+console.log("Object:", myObject);
+
+// 7. Array
+const myArray = [1, 2, 3, 4, 5];
+console.log("Array:", myArray);
